ac944492e2a10f70f1f51354522eb9ec36c33c79
ELITON NASCIMENTO <122913170+Eliton03@users.noreply.github.com>
2025-10-20 15:50:45 +0000

    verificação de qual numero é maior entre dois números.

---
+++ Main/Estrutura.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main () { 
+    int numero1, numero2;
+
+    numero1 = 21;
+    numero2 = 20;
+  
+    
+    if (numero1 > numero2)
+        printf("Numero 1 e maior que Numero 2\n");
+      else 
+        printf("Numero 1 nao e maior que Numero 2\n");
+    
+}

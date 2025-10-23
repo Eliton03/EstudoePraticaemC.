da14cc35e99c01b1c1195aedd0a2dbc4be2bd87a
ELITON NASCIMENTO <122913170+Eliton03@users.noreply.github.com>
2025-10-23 17:59:55 +0000

    codigo para aplicar desconto especial

--- Main/aplicardesconto.c
+++ Main/aplicardesconto.c
@@ -4,27 +4,22 @@ int main() {
     int idade;
     float renda_mensal;
 
-/* programa para verificar se uma pessoa está qualificda para um desconto 
-especial com base mna idade e na renda mensal. 
-A Pessoa deve ter 65 anos ou mais ou uma renda mensal inferior a R$ 2000,00
-*/
+    /* Programa para verificar se uma pessoa está qualificada
+       para um desconto especial com base na idade e na renda mensal.
+       A pessoa deve ter 65 anos ou mais OU uma renda mensal inferior a R$ 2000,00.
+    */
 
-printf("digite a sua idade: ");
-scanf("%d", &idade);
-printf("digite a sua renda mensal: ");
-scanf("%f", &renda_mensal);
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
 
-if(idade <= 18 || idade >= 65)
-{
-    if (renda_mensal < 2000.00)
-    {
-      printf(" Você tem direito ao desconto!\n");
+    printf("Digite a sua renda mensal: ");
+    scanf("%f", &renda_mensal);
+
+    if (idade >= 65 || renda_mensal < 2000.00) {
+        printf("Você tem direito ao desconto!\n");
     } else {
-      printf("Você não tem direito ao desconto, devido á renda!\n");
+        printf("Você não tem direito ao desconto.\n");
     }
-    printf("Aceito com relação a idade");
-} else {
-    printf("Você não está aceito com relação a idade\n");
-}
+
     return 0;
 }
